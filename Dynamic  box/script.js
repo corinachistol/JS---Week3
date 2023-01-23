4271aa94a746a7f77c891f3e7a8e0465692de271
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-01-23 17:00:44 +0200

    Add files via upload

--- Dynamic  box/script.js
+++ Dynamic  box/script.js
@@ -1,70 +1,61 @@
 //global variable
 let size = 100  //number
+let color1 = red
+let color2 = blue
+let color3 = yellow
+
+
+
+function showBox(){
+   container.innerHTML = `
+      <div 
+      class="box"
+      style="
+         width:${size}px;
+         height:${size}px;
+         background-color:red;
+      "
+      ></div>
+
+   `
+}
 
 
 //declare a function
  function increase() {
    size += 10
    console.log(size)
-   container.innerHTML = `
-      <div 
-      class="box"
-      style="
-         width:${size}px;
-         height:${size}px;
-      "
-      ></div>
-
-   `
+   showBox()
 }
 
 function decrease(){
    size -= 10
    console.log(size)
-   container.innerHTML = `
-      <div 
-      class="box"
-      style="
-         width:${size}px;
-         height:${size}px;
-      "
-      ></div>
-
-   `
+   showBox()
     
 }
 
+
 function red() {
-   container.innerHTML = `
-   <div
-   class="box"
-   style="background-color:red;"
-   ></div>
-   `
+  //let color = red
+   console.log(color1)
+   showBox()
    
 }
 
 
-
 function blue(){
-   container.innerHTML = `
-   <div 
-   class="box"
-   style="background-color:blue;"
-   ></div>
-
-   `
+   let color = blue
+   console.log(color)
+   showBox()
 
 }
 
-function yellow(){
-   container.innerHTML =  `
-   <div 
-   class="box"
-   style="background-color:yellow;"
-   ></div>
 
-   `
+function yellow(){
+   //let color = yellow
+   console.log(`${color3}`)
+   showBox()
 }
 
 
